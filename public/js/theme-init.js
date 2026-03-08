c4a10fa5f42e0457977461bb1f1754a6629239e8
GolDRoger21 <ercan21@gmail.com>
2026-03-08 17:49:16 +0300

    aaa

--- public/js/theme-init.js
+++ public/js/theme-init.js
@@ -1,5 +1,14 @@
-import { applyTheme } from './theme-manager.js';
-
 (function applyInitialTheme() {
-  applyTheme();
+  var theme = null;
+  try {
+    theme = localStorage.getItem('theme');
+  } catch (e) {
+    theme = null;
+  }
+
+  if (theme !== 'light' && theme !== 'dark') {
+    theme = 'dark';
+  }
+
+  document.documentElement.setAttribute('data-theme', theme);
 })();
